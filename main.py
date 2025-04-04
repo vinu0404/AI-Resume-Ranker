f69fdd40174567fc87068dd0d5bb2cd0e8fef5a7
vinu0404 <22cd3034@rgipt.ac.in>
2025-04-04 22:08:19 +0530

    Streamlit code

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ import ast
 
 # AWS Bedrock setup
 bedrock = boto3.client(service_name="bedrock-runtime", region_name="us-east-1")  
-
-# Load the preprocessed resume dataset
-df_sampled = pd.read_csv("25rows_with_embeddings.csv")
+df_sampled = pd.read_csv("25rows_with_embeddings_per_category.csv")
 
 # Convert embedding strings back to lists
 df_sampled["embedding"] = df_sampled["embedding"].apply(ast.literal_eval)
@@ -35,13 +33,12 @@ def cosine_similarity(vec1, vec2):
 # Streamlit UI
 st.set_page_config(page_title="Resume Screening App", layout="wide")
 
-st.title("🔍 AI-Powered Resume Screening")
+st.title("AI-Powered Resume Screening")
 st.write("Enter a job description and find the most relevant resumes!")
 
 # Input box for job query
-job_query = st.text_area("Enter Job Description", placeholder="e.g.,Machine Learning,Data Science & Have Intership In Domain Of Machine Learning")
+job_query = st.text_area("Enter Job Description", placeholder="e.g.,Machine Learning,Data Science ,SQL  & Have experience of 5 years")
 
-# Submit button
 if st.button("Find Matching Resumes"):
     if job_query:
         # Generate embedding for job query
@@ -54,9 +51,7 @@ if st.button("Find Matching Resumes"):
         # Rank resumes
 
         df_ranked = df_sampled.sort_values(by="similarity_score", ascending=False).head(10)
-
-        # Display top resumes
-        st.subheader("🏆 Top Matching Resumes")
+        st.subheader("Top Matching Resumes")
         st.dataframe(df_ranked[["ID", "Category", "similarity_score"]])
     else:
         st.warning("⚠️ Please enter a job description.")
